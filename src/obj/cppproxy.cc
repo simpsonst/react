fba89aaf7ab69dea8c97f3e2a53537afecc5699e
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2023-02-20 21:30:02 +0000

    Fixed contact details.

--- src/obj/cppproxy.cc
+++ src/obj/cppproxy.cc
@@ -18,7 +18,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  Contact Steven Simpson <http://www.lancaster.ac.uk/~simpsons/>
+ *  Contact Steven Simpson <https://github.com/simpsonst>
  */
 
 #include <cstdio>
